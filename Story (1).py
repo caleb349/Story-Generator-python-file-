bad113d157c328cf942e12a35e714f0f47f5c923
caleb349 <39037672+caleb349@users.noreply.github.com>
2019-10-28 19:28:48 -0400

    Add files via upload

---
+++ Story (1).py
@@ -0,0 +1,28 @@
+﻿# ask a user for their first name and store the input in a variable.
+First_name = input("what is your first Name? ")
+print('hello,nice to meet you', First_name, "!") 
+#ask a user for their last name and store the input in a variable.
+Last_name = input('what is your last_name? ')
+print('helloo',Last_name, "!")
+#ask a user for their addresss and store the input in a variable.
+addresss = input('what is your address? ')
+print('wow so you live at', addresss, "!")
+#ask a user for their city and store the input in a variable.
+City = input('what city do you live in? ')
+print('welcome',City, "!")
+#ask a user for their state and store the input in a variable.
+State = input('what state? ')
+print('welcome',State, "!")
+#asks a user for their zip code and store the input in a variable.
+Zip_code = input('what is your zip_code? ')
+print('welcome',Zip_code, "!")
+#ask a user what year they where born and store the input in a variable.
+Year = input('what year where you born? ')
+print('awesome',Year, "!")
+#ask a user for the name of a freind and store the input in a variable.
+freind = input('what is the name of your friend? ')
+print('hey',freind, "!")
+#Output all the variables in a short story of your choosing using concatenation. The story should be something fun and utilize the user's information in a creative way.
+print("ALL VARIABLES")
+print("This is a story generator", "This is ", First_name, Last_name,  "He live at ",addresss, City, State, Zip_code, Year, "His best friend is ", freind, "nice story glad to hear it")
+
